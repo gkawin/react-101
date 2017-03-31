72ff2595f1bb3eee1c185a54eb9b53b320794567
Kawin Glomjai <kawin.g@taskworld.com>
2017-04-01 00:05:51 +0700

    first thing you should do

---
+++ the-basic/webpack.config.js
@@ -0,0 +1,29 @@
+const Path = require('path')
+const BASE = Path.resolve(__dirname)
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: Path.join(BASE, 'index.js'),
+  output: {
+    path: Path.join(BASE, 'build'),
+    filename: 'bundle.js'
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ compress: {
+      warnings: false,
+      drop_console: true,
+      dead_code: true,
+    },
+      comments: false }),
+  ],
+}
